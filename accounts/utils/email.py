8a0bfbade93b46058d8fb17c3dd9499911ece0db
sugandv21 <sdvsuganya@gmail.com>
2026-02-05 11:29:07 +0530

    Update email.py

--- accounts/utils/email.py
+++ accounts/utils/email.py
@@ -1,19 +1,39 @@
-import logging
+# import logging
+# from django.core.mail import send_mail
+# from django.conf import settings
+
+# logger = logging.getLogger(__name__)
+
+# def safe_send_mail(subject, message, recipient_list):
+#     try:
+#         send_mail(
+#             subject=subject,
+#             message=message,
+#             from_email=settings.DEFAULT_FROM_EMAIL,
+#             recipient_list=recipient_list,
+#             fail_silently=False,
+#         )
+#         return True
+#     except Exception as e:
+#         logger.error(f"EMAIL FAILED: {e}")
+#         return False
+
 from django.core.mail import send_mail
 from django.conf import settings
+import logging
 
 logger = logging.getLogger(__name__)
 
 def safe_send_mail(subject, message, recipient_list):
     try:
         send_mail(
-            subject=subject,
-            message=message,
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=recipient_list,
+            subject,
+            message,
+            settings.DEFAULT_FROM_EMAIL,
+            recipient_list,
             fail_silently=False,
         )
         return True
     except Exception as e:
-        logger.error(f"EMAIL FAILED: {e}")
+        logger.error(f"MAIL ERROR: {e}")
         return False
