e93e0f3f1e2bddb6e7f536294d171f35e96bb8d8
sugandv21 <sdvsuganya@gmail.com>
2026-02-06 11:43:38 +0530

    Update settings.py

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -211,3 +211,14 @@ DEFAULT_FROM_EMAIL = os.environ.get(
     "DEFAULT_FROM_EMAIL",
     EMAIL_HOST_USER
 )
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "root": {"handlers": ["console"], "level": "ERROR"},
+}
+
+
