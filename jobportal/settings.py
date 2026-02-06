e588d36de0a518a92166a9d6479f2e974ed73047
sugandv21 <sdvsuganya@gmail.com>
2026-02-06 12:03:03 +0530

    Update settings.py

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -198,13 +198,15 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 #     EMAIL_HOST_USER
 # )
 
-EMAIL_BACKEND=django.core.mail.backends.smtp.EmailBackend
-EMAIL_HOST=smtp.gmail.com
-EMAIL_PORT=587
-EMAIL_USE_TLS=True
-EMAIL_HOST_USER=suganyasdv16@gmail.com
-EMAIL_HOST_PASSWORD=duczripxqitpjvbu
-DEFAULT_FROM_EMAIL=suganyasdv16@gmail.com
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = "suganyasdv16@gmail.com"
+EMAIL_HOST_PASSWORD = "duczripxqitpjvbu"
+DEFAULT_FROM_EMAIL = "suganyasdv16@gmail.com"
+EMAIL_TIMEOUT = 20
 
 # EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 # EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
@@ -222,6 +224,7 @@ LOGGING = {
     },
     "root": {"handlers": ["console"], "level": "ERROR"},
 }
+
 
 
 
