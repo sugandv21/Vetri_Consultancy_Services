ffdd08a3af57fe4e6de60952f830cc68c12a607c
sugandv21 <sdvsuganya@gmail.com>
2026-02-12 18:08:54 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -208,7 +208,7 @@ def payment(request):
             user.plan = selected_plan
             user.plan_status = User.ACTIVE
             user.plan_start = timezone.now()
-            user.plan_end = timezone.now() + timedelta(minutes=2)
+            user.plan_end = timezone.now() + timedelta(days=30)
             user.save()
 
             Payment.objects.create(
@@ -1038,3 +1038,4 @@ def mark_alert_read(request, alert_id):
     alert.is_read = True
     alert.save()
     return redirect("admin_unread_alerts")
+
